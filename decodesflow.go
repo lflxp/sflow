72c6a44ba396acdcde387c1f1db4dc5283d7a7d0
李学坪 <lixueping@lixuepingdeiMac.local>
2017-11-13 17:32:45 +0800

    add sample only for test

--- decodesflow.go
+++ decodesflow.go
@@ -1,21 +1,22 @@
 package sflow
 
 import (
-	"fmt"
-	"encoding/json"
+	//"fmt"
+	//"encoding/json"
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 )
 
-func DecodeSflow(tuple *Datagram, payload []byte) ([]string, error) {
-	result := []string{}
+func DecodeSflow(tuple *Datagram, payload []byte) ([]*FlowSamples,[]*SFlowCounterSample, error) {
+	samples := []*FlowSamples{}
+	counters := []*SFlowCounterSample{}
 	Header := NewData()
 	Header.InitDatagram(tuple)
 	Header.Init(payload)
 
 	pp := gopacket.NewPacket(payload, layers.LayerTypeSFlow, gopacket.Default)
 	if pp.ErrorLayer() != nil {
-		return nil, pp.ErrorLayer().Error()
+		return nil,nil, pp.ErrorLayer().Error()
 	}
 	if got, ok := pp.ApplicationLayer().(*layers.SFlowDatagram); ok {
 		if len(got.FlowSamples) > 0 {
@@ -24,12 +25,13 @@ func DecodeSflow(tuple *Datagram, payload []byte) ([]string, error) {
 				Header.Type = "sample"
 				tmp.Data = Header
 				tmp.InitFlowSampleData(y)
-				b, err := json.Marshal(tmp)
-				if err != nil {
-					fmt.Println(err.Error())
-				}
-
-				result = append(result, string(b))
+				//b, err := json.Marshal(tmp)
+				//if err != nil {
+				//	fmt.Println(err.Error())
+				//}
+				//
+				//samples = append(samples, string(b))
+				samples = append(samples,tmp)
 			}
 		}
 
@@ -41,13 +43,45 @@ func DecodeSflow(tuple *Datagram, payload []byte) ([]string, error) {
 				tmp.InitCounterSample(y)
 			}
 
-			b, err := json.Marshal(tmp)
-			if err != nil {
-				fmt.Println(err.Error())
-			}
-
-			result = append(result, string(b))
+			//b, err := json.Marshal(tmp)
+			//if err != nil {
+			//	fmt.Println(err.Error())
+			//}
+			//
+			//counters = append(counters, string(b))
+			counters = append(counters,tmp)
 		}
 	}
-	return result, nil
+	return samples,counters, nil
+}
+
+func DecodeSample(tuple *Datagram, payload []byte) ([]*FlowSamples, error) {
+	samples := []*FlowSamples{}
+	Header := NewData()
+	Header.InitDatagram(tuple)
+	Header.Init(payload)
+
+	pp := gopacket.NewPacket(payload, layers.LayerTypeSFlow, gopacket.Default)
+	if pp.ErrorLayer() != nil {
+		return nil,nil, pp.ErrorLayer().Error()
+	}
+	if got, ok := pp.ApplicationLayer().(*layers.SFlowDatagram); ok {
+		if len(got.FlowSamples) > 0 {
+			for _, y := range got.FlowSamples {
+				tmp := NewFlowSamples()
+				Header.Type = "sample"
+				tmp.Data = Header
+				tmp.InitFlowSampleData(y)
+				//b, err := json.Marshal(tmp)
+				//if err != nil {
+				//	fmt.Println(err.Error())
+				//}
+				//
+				//samples = append(samples, string(b))
+				samples = append(samples,tmp)
+			}
+		}
+
+	}
+	return samples, nil
 }
